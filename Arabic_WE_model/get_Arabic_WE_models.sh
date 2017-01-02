a06bddaa761e61eb37a35f4596bb330db8fb987a
DAHOU Abdelghani <dahou.abdghani@gmail.com>
2017-01-02 11:46:59 +0800

    Add files via upload

---
+++ Arabic_WE_model/get_Arabic_WE_models.sh
@@ -0,0 +1,9 @@
+#Word Embeddings and Convolutional Neural Network for Arabic Sentiment Classification
+# Paper: https://www.aclweb.org/anthology/C/C16/C16-1228.pdf
+# Other models: http://pan.baidu.com/s/1eS2mxCe
+
+echo "Downloading CBOW58 model:"
+echo ""
+wget https://goo.gl/pCDC45
+echo ""
+echo "Done."
